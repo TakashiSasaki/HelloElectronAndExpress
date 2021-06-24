7e6575d20460a2cafd38923fb391c945077512e6
Takashi SASAKI <takashi316@gmail.com>
2021-06-24 21:03:32 +0900

    Create electron.js

---
+++ electron.js
@@ -0,0 +1,20 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+app.on('ready', () => {
+  // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
+  mainWindow = new BrowserWindow({width: 400, height: 300});
+  // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ChromiumのDevツールを開く
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+//https://qiita.com/umamichi/items/6ce4f46c1458e89c4cfc
+//からコピペ
